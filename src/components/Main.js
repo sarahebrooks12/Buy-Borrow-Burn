8dd66df05bc1ebed09323fb0b7f3b6a7be6b032b
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-06-18 15:43:29 -0400

    nav bar again

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,9 @@ import React, { Component } from "react"
 import NavBar from "./nav/NavBar"
 import ApplicationViews from "./ApplicationViews"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'fontsource-roboto';
+// import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+// import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
 
 
 class BBB extends Component {
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,25 +1,82 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./NavBar.css";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-
-class NavBar extends Component {
-  render() {
-    return (
-      <header>
-        <Navbar bg="dark" variant="dark" id="navBar">
-          <Navbar.Brand href="/"><b>B</b>uy, <b>B</b>orrow, <b>B</b>uy</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/news">My Bookshelf</Nav.Link>
-            <Nav.Link href="/events">Friends' Bookshelves</Nav.Link>
-            <Nav.Link href="/tasks">Search</Nav.Link>
-          </Nav>
-        </Navbar>
-      </header>
-    );
-  }
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import BookIcon from '@material-ui/icons/Book';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import MailIcon from '@material-ui/icons/Mail';
+const drawerWidth = 125;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
+export default function PermanentDrawerLeft() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            <b>B</b>uy, <b>B</b>orrow, <b>B</b>urn
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {['Home', 'My Bookshelf', 'Browse Books', 'Friends'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <MenuBookIcon /> : <BookmarkBorderIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+        </Typography>
+        <Typography paragraph>
+        </Typography>
+      </main>
+    </div>
+  );
 }
-
-export default NavBar;
