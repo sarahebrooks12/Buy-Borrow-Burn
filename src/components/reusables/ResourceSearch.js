f8ab14e1681a0aff4264f0f3462a5502466584c7
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-06-22 15:43:53 -0400

    capstone

--- src/components/reusables/ResourceSearch.js
+++ src/components/reusables/ResourceSearch.js
@@ -1,57 +1,19 @@
 import React from 'react';
 class Search extends  React.Component {
-	constructor( props ) {
-		super( props );
-		this.state = {
-			query: '',
-                        results: {},
-                        loading: false,
-                        message: '',
-		};
-	}
-	render() {
-		return (
-			<div className="container">
-				{/*Heading*/}
-				<h2 className="heading">Live Search: React Application</h2>
-				{/*Search Input*/}
-				<label className="search-label" htmlFor="search-input">
-					<input
-						type="text"
-						value=""
-						id="search-input"
-						placeholder="Search..."
-					/>
-					<i className="fa fa-search search-icon"/>
-				</label>
-				
-			</div>
-			)
-	}
-}
-export default Search;
 
-
-
-
-componentDidMount() {
-    BookManager.getSearch(userSearch).then((books) => {
-      this.setState({
-        books: books,
-      });
-    });
-  }
-  <input
+  
+render() {
+    return (
+        <input
                 type="text"
                 required
                 className="form-control"
                 onChange={this.handleFieldChange}
-                id="name"
-                value={this.state.title}
+                id="animalName"
+                value={this.state.animalName}
               />
-// search function? 
-  handleFieldChange = (books) => {
-    const stateToChange = {};
-    stateToChange[books.target.id] = books.target.value;
-    this.setState(stateToChange);
-  };
+    )}
+      
+    
+}
+export default Search
