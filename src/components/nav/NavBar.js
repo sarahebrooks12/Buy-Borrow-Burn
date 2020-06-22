c6cf1f6551b4f8a28cb2ba2ea89ef5649f3df183
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-06-22 15:43:53 -0400

    capstone

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,13 +3,22 @@ import { Input, Label, Menu } from "semantic-ui-react";
 import { NavLink, withRouter } from "react-router-dom";
 
 export default class NavBar extends Component {
-  state = { activeItem: "inbox" };
+  state = { 
+    activeItem: "myBookshelf"
+   };
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  handleFieldChange = evt => {
+    const stateToChange = {}
+    stateToChange[evt.target.id] = evt.target.value
+    this.setState(stateToChange)
+  }
 
   render() {
     const { activeItem } = this.state;
 
+ 
     return (
       <Menu vertical>
         <Menu.Item
@@ -44,7 +53,10 @@ export default class NavBar extends Component {
           Following
         </Menu.Item>
         <Menu.Item>
-          <Input icon="search" placeholder="Search bookshelves..." />
+          <Input icon="search"
+          placeholder="Search bookshelves..."
+          onChange={this.handleFieldChange} 
+          />
         </Menu.Item>
       </Menu>
     );
