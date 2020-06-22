c0f17d1c1e3e9f459aa74353a8c11a5a9738cf87
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-06-22 13:41:46 -0400

    nav bar yes

--- src/modules/BookManager.js
+++ src/modules/BookManager.js
@@ -1,13 +1,12 @@
-const remoteURL = "http://localhost:8088"
+const remoteURL = "http://localhost:8088";
 
 export default {
-  get(searchBar) {
-    return fetch(`https://www.goodreads.com/search/index.xml?key=w1FqGS3nZhLHMl1510ckxQ?q=${searchBar}`).then(result => result.json())
+  getSearch(userSearch, googleBooksKey) {
+    return fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${userSearch}&key=${googleBooksKey}`
+    ).then((result) => result.json());
   },
-  getHarry(){
-        return fetch(`https://www.googleapis.com/books/v1/volumes?q=drseuss&key=AIzaSyBiqZBaqAZBbOixMmphbqDrq1P5yjtWhTY`).then(result => result.json())
+  getAll(id) {
+    return fetch (`${remoteURL}/myBookshelf/${id}`).then(result => result.json())
   }
-}
-
-// https://app.ticketmaster.com/discovery/v2/events.json?dmaId=343&keyword=${concertSearchBar}&apikey=${apiToken}
-
+};
