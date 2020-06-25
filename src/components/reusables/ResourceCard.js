4376866532879e798a8a5fd97815f8c1c7a46a0b
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-06-25 09:42:08 -0400

    test

--- src/components/reusables/ResourceCard.js
+++ src/components/reusables/ResourceCard.js
@@ -7,28 +7,36 @@ class ResourceCard extends React.Component {
       <Grid divided="vertically">
         <Grid.Row columns={1}>
           <Grid.Column>Favorite</Grid.Column>
-          <Card>
-    <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
+          <div class="test" id="searchCard">
+            <Card.Group itemsPerRow={4}>
+              <Card>
+                <Card.Content>
+                  <Card.Header>Title</Card.Header>
+                  <Card.Meta>
+                    <span>Author</span>
+                  </Card.Meta>
+                  <a>
+                    <Icon name="star" />
+                    
+                    <br />
+                    {/* if rating = this then print this ternary statement */}
+                    <Icon name="dollar" />
+                    
+                    <br />
+                    <Icon name="heart outline" />
+                   
+                    <br />
+                    <Icon name="fire" />
+                   
+                  </a>
+                </Card.Content>
+              </Card>
+            </Card.Group>
+          </div>
         </Grid.Row>
 
         <Grid.Row columns={1}>
-          <Grid.Column>To Read</Grid.Column>
+          <Grid.Column >To Read</Grid.Column>
         </Grid.Row>
 
         <Grid.Row columns={1}>
