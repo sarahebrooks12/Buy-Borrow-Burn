e741523b5350e12b4f83cff126863be966255480
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-06-26 15:48:56 -0400

    edit/delete need to work

--- src/components/books/BookList.js
+++ src/components/books/BookList.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 //import the components we will need
-import BookCard from "./BookCard";
+import BookShelf from "./BookShelf";
 import BookManager from "../../modules/BookManager";
 // import { Button, Icon } from "semantic-ui-react";
 
 class BookList extends Component {
-  state = {
+ state = {
     books: [],
+    ratings: []
   };
-
   componentDidMount() {
+    BookManager.getRatings().then((ratings) => {
+      const rateOption = ratings.map(x => {
+
+        return ({
+         key: x.id,
+         text: x.name,
+         value: x.id
+       })
+      })
+      this.setState({
+        ratings: rateOption,
+      });
+    });
     BookManager.getAllBooks().then((books) => {
       this.setState({
         books: books,
@@ -18,17 +31,15 @@ class BookList extends Component {
   }
 
   render() {
-    //     this.state.books.sort((a, b) => {
-    //       return a.date > b.date ? 1 : -1;
-    //    });
 
     return (
       <>
         {this.state.books.map((currentBookInLoop) => {
           return (
-            <BookCard
+            <BookShelf
               key={currentBookInLoop.id}
               bookProp={currentBookInLoop}
+              ratingProp={this.state.ratings}
             />
           );
         })}
