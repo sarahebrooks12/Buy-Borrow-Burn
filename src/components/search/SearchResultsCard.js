4ab9c4d0baf21f15b11cd9e35a1fe2459ee0e617
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-07-02 16:02:36 -0400

    CSS

--- src/components/search/SearchResultsCard.js
+++ src/components/search/SearchResultsCard.js
@@ -9,6 +9,7 @@ import {
   Header,
   Checkbox,
   Dropdown,
+  Grid,
 } from "semantic-ui-react";
 import "./SearchCard.css";
 import BookManager from "../../modules/BookManager";
@@ -18,7 +19,7 @@ import BookManager from "../../modules/BookManager";
 class SearchResultsCard extends React.Component {
   state = {
     ratingId: "",
-    favorite: false
+    favorite: false,
   };
 
   createNewBook = (evt) => {
@@ -38,114 +39,112 @@ class SearchResultsCard extends React.Component {
     );
   };
 
- handleDropDownChange= (event, {value}) => {
-    this.setState({ratingId: value})
-}
-//evt.target.checked=true or false
-//setState
-//set Favorite property in state to either true or false
-  handleFavorite = (event, {checked}) => {
+  handleDropDownChange = (event, { value }) => {
+    this.setState({ ratingId: value });
+  };
+  //evt.target.checked=true or false
+  //setState
+  //set Favorite property in state to either true or false
+  handleFavorite = (event, { checked }) => {
     this.setState({ favorite: checked });
-  }
-  
+  };
 
   render() {
     return (
       <>
         <div id="searchCard">
-          <Card.Group itemsPerRow={4}>
-            <Card>
-              {this.props.searchProp.volumeInfo.imageLinks ? (
-                <Image
-                  src={
-                    this.props.searchProp.volumeInfo.imageLinks.smallThumbnail
+                <Card id="cardSize">
+                  {this.props.searchProp.volumeInfo.imageLinks ? (
+                    <Image
+                    id="imageSize"
+                      src={
+                        this.props.searchProp.volumeInfo.imageLinks
+                          .smallThumbnail
+                      }
+                      wrapped
+                      size="small"
+                      ui={false}
+                    />
+                  ) : (
+                    <Image
+                    id="imageNotAvail"
+                      src={require("./No_Img_Avail.jpg")}
+                      alt="Not Available"
+                      size="small"
+                    />
+                  )}
+
+                  <Card.Content>
+                    <Card.Header>
+                      {this.props.searchProp.volumeInfo.title}
+                    </Card.Header>
+                    <Card.Meta>
+                      <span>{this.props.searchProp.volumeInfo.authors}</span>
+                    </Card.Meta>
+                    <div className="ui divider">
+                      <a>
+                        <Icon name="star" />
+                        {this.props.searchProp.volumeInfo.averageRating}
+                      </a>
+                    </div>
+                  </Card.Content>
+                </Card>
+                <Modal
+                  trigger={
+                    <Button span animated>
+                      <Button.Content visible>Add to Shelf</Button.Content>
+                      <Button.Content hidden>
+                        <Icon name="book" />
+                      </Button.Content>
+                    </Button>
                   }
-                  wrapped
-                  size="small"
-                  ui={false}
-                />
-              ) : (
-                <Image
-                  src={require("./No_Img_Avail.jpg")}
-                  alt="Not Available"
-                  size="small"
-                />
-              )}
+                >
+                  <Modal.Header>
+                    {this.props.searchProp.volumeInfo.title}
+                  </Modal.Header>
+                  <Modal.Content image>
+                    {this.props.searchProp.volumeInfo.imageLinks ? (
+                      <Image
+                        src={
+                          this.props.searchProp.volumeInfo.imageLinks
+                            .smallThumbnail
+                        }
+                        wrapped
+                        size="small"
+                        ui={false}
+                      />
+                    ) : (
+                      <Image
+                        src={require("./No_Img_Avail.jpg")}
+                        alt="Not Available"
+                        size="small"
+                      />
+                    )}
+                    <Modal.Description>
+                      <Header>
+                        {this.props.searchProp.volumeInfo.authors}
+                      </Header>
+                      {this.props.searchProp.volumeInfo.description}
+                    </Modal.Description>
+                  </Modal.Content>
 
-              <Card.Content>
-                <Card.Header>
-                  {this.props.searchProp.volumeInfo.title}
-                </Card.Header>
-                <Card.Meta>
-                  <span>{this.props.searchProp.volumeInfo.authors}</span>
-                </Card.Meta>
-                <div className="ui divider">
-                  <a>
-                    <Icon name="star" />
-                    {this.props.searchProp.volumeInfo.averageRating}
-                  </a>
-                </div>
-              </Card.Content>
-            </Card>
-          </Card.Group>
-        </div>
-        <div>
-          <Modal
-            trigger={
-              <Button animated>
-                <Button.Content visible>Add to Shelf</Button.Content>
-                <Button.Content hidden>
-                  <Icon name="book" />
-                </Button.Content>
-              </Button>
-            }
-          >
-            <Modal.Header>
-              {this.props.searchProp.volumeInfo.title}
-            </Modal.Header>
-            <Modal.Content image>
-              {this.props.searchProp.volumeInfo.imageLinks ? (
-                <Image
-                  src={
-                    this.props.searchProp.volumeInfo.imageLinks.smallThumbnail
-                  }
-                  wrapped
-                  size="small"
-                  ui={false}
-                />
-              ) : (
-                <Image
-                  src={require("./No_Img_Avail.jpg")}
-                  alt="Not Available"
-                  size="small"
-                />
-              )}
-              <Modal.Description>
-                <Header>{this.props.searchProp.volumeInfo.authors}</Header>
-                {this.props.searchProp.volumeInfo.description}
-              </Modal.Description>
-            </Modal.Content>
+                  <Checkbox label="Favorite" onChange={this.handleFavorite} />
+                  <Dropdown
+                    placeholder="Select Rating"
+                    fluid
+                    selection
+                    options={this.props.ratingProp}
+                    onChange={this.handleDropDownChange}
+                  />
+                  <br />
 
-            <Checkbox
-            label="Favorite"
-            onChange={this.handleFavorite}
-            />
-            <Dropdown
-              placeholder="Select Rating"
-              fluid
-              selection
-              options={this.props.ratingProp}
-              onChange={this.handleDropDownChange}
-            />
-            <br />
-
-            <Button animated onClick={this.createNewBook}>
-              <Button.Content visible>Add to Shelf</Button.Content>
-              <Button.Content hidden>
-                <Icon name="book" />
-              </Button.Content>
-            </Button>
-          </Modal>
+                  <Button animated onClick={this.createNewBook}>
+                    <Button.Content visible>Add to Shelf</Button.Content>
+                    <Button.Content hidden>
+                      <Icon name="book" />
+                    </Button.Content>
+                  </Button>
+                </Modal>
         </div>
       </>
     );
