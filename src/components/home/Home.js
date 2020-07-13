0e99bafde584056bd9bf13a342331afbe06e37e7
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-07-12 20:38:45 -0400

    styling done

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,19 +1,35 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 import { Image } from "semantic-ui-react";
-import "./Home.css"
+import BookManager from "../../modules/BookManager";
+import "./Home.css";
 
 class Home extends Component {
+  state = {
+   users: ""
+  };
+
+  componentDidMount() {
+    BookManager.getAllUsers().then((users) => {       
+      this.setState({
+        users: users,
+      });
+    });
+  }
+
   render() {
     return (
       <div id="backgroundImage">
-          <h3 id="homePage">
-            BUY BORROW BURN
-          </h3>
-          <p>Most of us enjoyed playing 'Kiss, Marry, Kill' as adolescents. Our friends would give us three people to choose between and you had to go through and make the choice between what you would do with each person. <br />
-          Now you can do the same thing but with the books you've read. <br />
-          When you rate a book on the common star system, it's hard to know what to purchase or whether to try and find a copy at your local library. Is it worth reading it to talk about the hype of a popular title or is it life changing? <br />
-          You can keep track of books that you want to read, currently reading, your favorites and then BBB. Would you spend your hand earned cash on a book or would you rather just borrow it from someone else? And my personal favorite category, burn - books you think should've never existed.  </p>
-        </div>
+        <h3 id="homePage">
+          <Image
+            fluid={true}
+            // centered={true}
+            id="Heading"
+            src={require("./BuyBorrowBurn.png")}
+            alt="Logo"
+          />
+         {/* <h1>Welcome {this.state.users.name}</h1> */}
+        </h3>
+      </div>
     );
   }
 }
